62337dd3a2bc3d4fdaa315ca8ba0136908939667
DemisoDaba <demo.nkmt1@gmail.com>
2023-06-24 05:23:00 -0700

    Updated

--- factors.py
+++ factors.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 
 import sys
+import math
 
 def factorize(number):
     factors = []
-    for i in range(2, int(number**0.5) + 1):
-        while number % i == 0:
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
             factors.append((i, number // i))
-            number //= i
-    if number > 1:
-        factors.append((number, 1))
     return factors
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: factors <file>")
-        return
-
-    filename = sys.argv[1]
-
-    with open(filename, 'r') as file:
-        numbers = file.read().splitlines()
-
-    for number in numbers:
-        number = int(number)
-        factor_pairs = factorize(number)
-        for factor_pair in factor_pairs:
-            p, q = factor_pair
-            print(f"{number} = {p} * {q}")
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            factor_pairs = factorize(number)
+            for pair in factor_pairs:
+                p, q = pair
+                print(f"{number}={p}*{q}")
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python factors.py <file>")
+        sys.exit(1)
 
+    file_path = sys.argv[1]
+    process_file(file_path)
